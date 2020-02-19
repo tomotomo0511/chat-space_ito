80fc2aed9179388ff4828cd28534c71b25391f67
tomotomo0511 <tomokaru0511@gmail.com>
2020-02-19 15:46:22 +0900

    chat-space_マイグレーションファイル編集

--- db/migrate/20200211122200_create_group_users.rb
+++ db/migrate/20200211122200_create_group_users.rb
@@ -1,6 +1,6 @@
 class CreateGroupUserrails < ActiveRecord::Migration[5.0]
   def change
-    create_table :group_userrails do |t|
+    create_table :group_users do |t|
       t.references :group, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
